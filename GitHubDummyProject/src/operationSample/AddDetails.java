acbaac9fc658b84dabd815d129f66230cc4f0364
Jeevanvishwa <jeevanvishwa155@gmail.com>
2024-07-25 12:31:58 +0530

    COMMIT MESSAGE

---
+++ GitHubDummyProject/src/operationSample/AddDetails.java
@@ -0,0 +1,16 @@
+package operationSample;
+
+public class AddDetails {
+
+	public static void main(String[] args) {  
+		String Name="rasma";
+		long Phno=8776655432l;
+		String Address="kanyakumari";
+		
+		System.out.println(Name);
+		System.out.println(Phno);
+		System.out.println(Address);
+
+	}
+
+}
